9321b507a8181081f1bc9ab63e9438ef228b466d
Munir Jojo Verge <munirjojoverge@yahoo.es>
2025-09-07 13:08:55 +0100

    fix: changing headers in all files

--- interbotix_ros_xslocobots/examples/interbotix_xslocobot_joy/scripts/xslocobot_robot.py
+++ interbotix_ros_xslocobots/examples/interbotix_xslocobot_joy/scripts/xslocobot_robot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2022 Trossen Robotics
+# Copyright 2025 Cortical Robotics
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
